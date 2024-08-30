4520bb5ccef87c99eb30de9f55aa7fde7d12ef12
Yuganshu <yuganshu219@gmail.com>
2024-08-30 10:26:01 +0530

    Modify 'PokemonStat.svelte' to show red color when stat value is less than 50 points.

--- src/components/PokemonStat.svelte
+++ src/components/PokemonStat.svelte
@@ -10,8 +10,10 @@
 		"ri-speed-up-line",
 	];
 
-	let statBarColor = "bg-red-600 text-red-600";
-	if (value < 100) {
+	let statBarColor = "bg-gray-400 text-gray-400";
+	if (value < 50) {
+		statBarColor = "bg-red-600 text-red-600";
+	} else if (value < 100) {
 		statBarColor = "bg-orange-400 text-orange-400";
 	} else if (value < 150) {
 		statBarColor = "bg-green-400 text-green-400";
